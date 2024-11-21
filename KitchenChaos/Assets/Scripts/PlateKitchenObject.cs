f69f57de59fe14853d3d6c6b80da6256138911e7
paradisemom <andy774583558@gmail.com>
2024-11-21 19:49:11 +0800

    plate complete visual

--- KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs:EventArgs{
+        public KitchenObjectSO kitchenObjectSO;
+    }
     [SerializeField]private List<KitchenObjectSO> validKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
     private void Awake() {
@@ -19,6 +25,10 @@ public class PlateKitchenObject : KitchenObject
             return false;
         }else{
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this,new OnIngredientAddedEventArgs{
+                kitchenObjectSO=kitchenObjectSO
+            });
             return true;
         }
         
